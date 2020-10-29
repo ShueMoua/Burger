90a4073b2fe37f73e995b14e4b4952d214178038
Shuelong Moua <moua_shuelong@yahoo.com>
2020-10-29 00:19:15 -0500

    created connection with mysql

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ CREATE database burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     burger_name VARCHAR(30),
     devoured BOOLEAN
 );
