3c89b1d78c7c78cbe1543233a7b48d52d3a7d15b
Shuelong Moua <moua_shuelong@yahoo.com>
2020-10-29 00:06:52 -0500

    created database and sever.js files

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Jucy Lucy', false);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Good Burger', false);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Krabby Patty', false);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Sushi Burger', false);
