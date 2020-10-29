cac111e3aeb162cc5287476e1a663474ee1db1ba
Shuelong Moua <moua_shuelong@yahoo.com>
2020-10-29 00:43:41 -0500

    created controllers file

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const router = express.Router();
+
+// Import the model (cat.js) to use its database functions.
+const burger = require("../models/burger.js");
+
+
+
+
+
+
+module.exports = router;
