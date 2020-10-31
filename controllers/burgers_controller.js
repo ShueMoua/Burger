19a72c4a566f870b518c7272aa7c6501937aacca
Shuelong Moua <moua_shuelong@yahoo.com>
2020-10-30 20:25:59 -0500

    updating files to make orm/button functions work

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,9 +5,40 @@ const router = express.Router();
 // Import the model (cat.js) to use its database functions.
 const burger = require("../models/burger.js");
 
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+        let hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
 
+router.post("/api/burgers", function(req, res) {
+    burger.insertOne([
+        "burger_name"
+    ], [
+        req.body.name
+    ], function(result) {
+        res.json({ id: result.insertid });
+    });
+});
 
+router.put("/api/burgers/:id", function(req, res) {
+    let condition = "id = " + req.params.id;
 
+    console.log("condition", condition);
 
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function(result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
 
 module.exports = router;
