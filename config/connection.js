a3eaf15856c4ceaf7eaa10c765ca1d47fe735e36
Shuelong Moua <moua_shuelong@yahoo.com>
2020-12-02 20:45:33 -0600

    deploying

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,27 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host: "jw0ch9vofhcajqg7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    port: 3306,
-    user: "bfe5zhncz72gyt8e",
-    password: "bbm0x7i90hsb4dj0",
-    database: "f4yoscukp3vr4gi5"
-});
 
-connection.connect(function(err) {
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "root",
+        database: "burgers_db"
+    });
+};
+
+// const connection = mysql.createConnection({
+//     host: "jw0ch9vofhcajqg7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//     port: 3306,
+//     user: "bfe5zhncz72gyt8e",
+//     password: "bbm0x7i90hsb4dj0",
+//     database: "f4yoscukp3vr4gi5"
+// });
+
+connection.connect(function (err) {
     if (err) {
         console.error("error connecting: " + err.stack);
         return;
@@ -18,15 +31,15 @@ connection.connect(function(err) {
 
 module.exports = connection;
 
-/* 
+/*
 Connection to Heroku
-host: jw0ch9vofhcajqg7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	
+host: jw0ch9vofhcajqg7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com
 
 Connection Username: bfe5zhncz72gyt8e
 
 password: bbm0x7i90hsb4dj0
 
-database: f4yoscukp3vr4gi5 
+database: f4yoscukp3vr4gi5
 
 
 Connection for localhost
@@ -36,5 +49,7 @@ Connection for localhost
     user: "root",
     password: "root",
     database: "burgers_db"
-    
+
+    desolate-coast-57642
+
     */
